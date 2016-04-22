8e17b8fd6c935a384160dc54e7c231c8ecd0590c
xutaoding <xutao.ding@chinascopefinancial.com>
2016-04-22 14:41:28 +0800

    fix bugs

--- statistics/django_uwsgi.py
+++ statistics/django_uwsgi.py
@@ -1,10 +1,11 @@
 import os
 import sys
 
+import django
 from django.core.handlers.wsgi import WSGIHandler
 
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "statistics.settings")
-
+django.setup()
 application = WSGIHandler()
