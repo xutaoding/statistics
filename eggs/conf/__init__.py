acf67c6f858e6ab2505592bd6b4ae98089ab254f
xutaoding <xutao.ding@chinascopefinancial.com>
2016-04-22 15:11:06 +0800

    modify unresonable config

--- eggs/conf/__init__.py
+++ eggs/conf/__init__.py
@@ -1,7 +1,30 @@
 """
 API configs to this project
 """
-import sys
+import socket
 
-ENV = sys.platform.upper()[:3] != 'WIN'
 
+def make_dev_ip():
+    """
+    :return: the actual ip of the local machine.
+        This code figures out what source address would be used if some traffic
+        were to be sent out to some well known address on the Internet. In this
+        case, a Google DNS server is used, but the specific address does not
+        matter much.  No traffic is actually sent.
+    """
+    try:
+        _socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        _socket.connect(('8.8.8.8', 80))
+        address, port = _socket.getsockname()
+        _socket.close()
+        return address
+    except socket.error:
+        return '127.0.0.1'
+
+dev_host = ['54.223.52.50']
+
+
+if make_dev_ip() in dev_host:
+    ENV = True
+else:
+    ENV = False
--- eggs/conf/news/debug.py
+++ eggs/conf/news/debug.py
@@ -1,5 +1,13 @@
-DEBUG_FULL_NEWS_PATH = 'D:/temp/csf_news/'
-DEBUG_HOT_NEWS_PATH = 'D:/temp/csf_hot_news/'
+import sys
+
+_is_win = sys.platform[:3].upper() == 'WIN'
+
+if _is_win:
+    DEBUG_FULL_NEWS_PATH = 'D:/temp/csf_news/'
+    DEBUG_HOT_NEWS_PATH = 'D:/temp/csf_hot_news/'
+else:
+    DEBUG_FULL_NEWS_PATH = '/data/news/csf_news/'
+    DEBUG_HOT_NEWS_PATH = '/data/news/csf_hot_news/'
 
 DEBUG_MONGO_HOST = '192.168.250.208'
 DEBUG_MONGO_PORT = 27017
