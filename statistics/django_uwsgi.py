98f751a37c36f9e6c1a313d554ccd397f5954cf9
xutaoding <xutao.ding@chinascopefinancial.com>
2016-04-22 11:54:02 +0800

    add django_uwsgi module path

--- statistics/django_uwsgi.py
+++ statistics/django_uwsgi.py
@@ -1,6 +1,9 @@
 import os
+import sys
 
 from django.core.handlers.wsgi import WSGIHandler
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "statistics.settings")
 
