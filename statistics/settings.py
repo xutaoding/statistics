1739e140a9f2a4c354ec0123962f48fe90c0bc00
xutaoding <xutao.ding@chinascopefinancial.com>
2016-04-22 14:38:10 +0800

    fix bugs

--- statistics/settings.py
+++ statistics/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'statistics.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
