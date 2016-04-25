1ff09ec665b66c34320ba03ac15fe1d0468dac3c
xutaoding <xutao.ding@chinascopefinancial.com>
2016-04-25 13:53:49 +0800

    test

--- statistics/django_uwsgi.py
+++ statistics/django_uwsgi.py
@@ -5,6 +5,7 @@ import django
 from django.core.handlers.wsgi import WSGIHandler
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append('/root/.pyenv/versions/anaconda-2.3.0/lib/python2.7/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "statistics.settings")
 django.setup()
