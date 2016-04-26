f9456dc44536de33a1619fda060213845923c4ab
xutaoding <xutao.ding@chinascopefinancial.com>
2016-04-26 16:17:25 +0800

    fix to appenf project path to sys.path

--- statistics/django_uwsgi.py
+++ statistics/django_uwsgi.py
@@ -1,10 +1,11 @@
 import os
 import sys
+from os.path import dirname, abspath
 
 import django
 from django.core.handlers.wsgi import WSGIHandler
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(dirname(dirname(abspath(__file__))))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "statistics.settings")
 django.setup()
